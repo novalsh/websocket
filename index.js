b268c822f2a12bb07175c7e6212091fc84bad55d
novalsh <muhammad.harjana@binus.ac.id>
2023-05-10 22:30:29 +0700

    websocket

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const socketIO = require('socket.io');
 const path = require('path');
 const express = require('express');
 const http = require('http');
+const { Server } = require('socket.io');
 
 const app = express();
 const server = require('http').Server(app);
-const io = socketIO(server);
+const io = new Server(server);
 
 app.use(express.static("public"));
 
-app.listen(9000);
+io.on("connection", socket => {
+    console.log("New client connected");
+    socket.on("kirim-pesan", pesan => {
+        socket.broadcast.emit("pesan-baru", pesan)
+    });
+});
+
+server.listen(9000);
--- public/script.js
+++ public/script.js
@@ -2,6 +2,9 @@ const btn_kirim = document.getElementById("kirim");
 const input = document.querySelector("input");
 const div_display = document.querySelector(".container-pesan");
 
+const socket = io();
+socket.on("connect", () => console.log("Connected to server"));
+
 const createBubbleChat = chat => {
     const div_pesan = document.createElement("div");
     div_pesan.classList.add("pesan");
@@ -12,5 +15,12 @@ const createBubbleChat = chat => {
 btn_kirim.addEventListener("click", () => {
     const bubbleChat = createBubbleChat(input.value);
     div_display.appendChild(bubbleChat);
+    socket.emit("kirim-pesan", input.value);
     input.value = '';
+});
+
+socket.on("pesan-baru", pesan => {
+   const bubbleChat = createBubbleChat(pesan); 
+   bubbleChat.classList.add("pesan-r");
+   div_display.appendChild(bubbleChat);
 });
